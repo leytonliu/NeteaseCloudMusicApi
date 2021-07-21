849ccf8a1b84acc29f44aa1dde7936e939eaa870
刘立东 <lidong.liu@hand-china.com>
2021-07-21 09:39:55 +0800

    Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use((req, res, next) => {
       'Access-Control-Allow-Origin': req.headers.origin || '*',
       'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
       'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+      'Access-Control-Allow-Headers': '*',
       'Content-Type': 'application/json; charset=utf-8',
     })
   }
